6d8e19a24f049f74d11aab66832e9d4e7f041939
RimeT <dacong_54321@163.com>
2017-09-19 17:43:13 +0800

    Basic Baidu Map Show. No location.

--- app/src/main/java/vivo/learn/rime/pedomap/MainActivity.java
+++ app/src/main/java/vivo/learn/rime/pedomap/MainActivity.java
@@ -1,13 +1,54 @@
 package vivo.learn.rime.pedomap;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapView;
 
 public class MainActivity extends AppCompatActivity {
+
+    MapView mMapView = null;
+    BaiduMap mBaiduMap = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_main);
+
+        //Baidumap
+        mMapView = (MapView) findViewById(R.id.map_view_baidu);
+        mBaiduMap = mMapView.getMap();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMapView.onResume();
+        mBaiduMap.setBaiduHeatMapEnabled(true);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
     }
 }
